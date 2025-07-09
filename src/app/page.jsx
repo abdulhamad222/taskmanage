b9d7100a0281e4794bfb628adff9eddb7e0b31cf
abdulhamad222 <abdulhamad373@gmail.com>
2025-07-09 14:45:00 +0500

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { Eye, EyeOff } from 'lucide-react'; // Eye icons
+import { Eye, EyeOff } from 'lucide-react';
 
 export default function Home() {
   const router = useRouter();
